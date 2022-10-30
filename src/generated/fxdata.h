db1dc0da4434089c432b79cf58b1ec1f05b32a2d
Peter Brown <brow1067@gmail.com>
2022-10-30 08:46:46 -0600

    add guard portrait

--- src/generated/fxdata.h
+++ src/generated/fxdata.h
@@ -11,12 +11,12 @@ using uint24_t = uint32_t;
 
 // Initialize FX hardware using  FX::begin(FX_DATA_PAGE); in the setup() function.
 
-constexpr uint16_t FX_DATA_PAGE  = 0xfe22;
-constexpr uint24_t FX_DATA_BYTES = 122158;
+constexpr uint16_t FX_DATA_PAGE  = 0xfe21;
+constexpr uint24_t FX_DATA_BYTES = 122414;
 
 constexpr uint24_t MAPDATA = 0x000000;
 constexpr uint24_t PORTRAIT_IMG = 0x018000;
-constexpr uint24_t PLAYER_IMG = 0x018400;
-constexpr uint24_t ENEMY_IMG = 0x019400;
-constexpr uint24_t TILE_IMG = 0x019C00;
-constexpr uint24_t STRINGDATA = 0x01DC00;
+constexpr uint24_t PLAYER_IMG = 0x018500;
+constexpr uint24_t ENEMY_IMG = 0x019500;
+constexpr uint24_t TILE_IMG = 0x019D00;
+constexpr uint24_t STRINGDATA = 0x01DD00;
